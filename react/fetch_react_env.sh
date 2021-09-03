e44b1df23ff4fc6b8a2ee07c0c3b9d201bf139e2
William <Wintan@student.unimelb.edu.au>
2021-09-03 10:36:35 +1000

    Update AWS environment variable

--- react/fetch_react_env.sh
+++ react/fetch_react_env.sh
@@ -55,23 +55,24 @@ if [ -n "$1" ] && [ "$1" = "unset" ]; then
   return 0
 fi
 
-
-cog_user_pool_id=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_COG_USER_POOL_ID' | jq -r .Parameter.Value)
+# Some parameter is taken from data-portal-client
+# Reference: https://github.com/umccr/data-portal-client
+cog_user_pool_id=$(aws ssm get-parameter --name '/data_portal/client/cog_user_pool_id' | jq -r .Parameter.Value)
 if [[ "$cog_user_pool_id" == "" ]]; then
   echo "Halt, No valid AWS login session found. Please 'aws sso login --profile dev && export AWS_PROFILE=dev'"
   return 1
 fi
-cog_identity_pool_id=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_COG_IDENTITY_POOL_ID' | jq -r .Parameter.Value)
-cog_app_client_id_local=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_COG_APP_CLIENT_ID_LOCAL' | jq -r .Parameter.Value)
-cog_app_client_id_stage=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_COG_APP_CLIENT_ID_STAGE' | jq -r .Parameter.Value)
+cog_identity_pool_id=$(aws ssm get-parameter --name '/data_portal/client/cog_identity_pool_id' | jq -r .Parameter.Value)
+cog_app_client_id_local=$(aws ssm get-parameter --name '/data_portal/client/cog_app_client_id_local' | jq -r .Parameter.Value)
+cog_app_client_id_stage=$(aws ssm get-parameter --name '/sscheck/client/cog_app_client_id_stage' | jq -r .Parameter.Value)
 
-oauth_domain=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_OAUTH_DOMAIN' | jq -r .Parameter.Value)
-oauth_redirect_in_local=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_OAUTH_REDIRECT_IN_LOCAL' | jq -r .Parameter.Value)
-oauth_redirect_out_local=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_OAUTH_REDIRECT_OUT_LOCAL' | jq -r .Parameter.Value)
-oauth_redirect_in_stage=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_OAUTH_REDIRECT_IN_STAGE' | jq -r .Parameter.Value)
-oauth_redirect_out_stage=$(aws ssm get-parameter --name '/samplesheet-check/fe/react/REACT_APP_OAUTH_REDIRECT_OUT_STAGE' | jq -r .Parameter.Value)
+oauth_domain=$(aws ssm get-parameter --name '/data_portal/client/oauth_domain' | jq -r .Parameter.Value)
+oauth_redirect_in_local=$(aws ssm get-parameter --name '/data_portal/client/oauth_redirect_in_local' | jq -r .Parameter.Value)
+oauth_redirect_out_local=$(aws ssm get-parameter --name '/data_portal/client/oauth_redirect_out_local' | jq -r .Parameter.Value)
+oauth_redirect_in_stage=$(aws ssm get-parameter --name '/sscheck/client/oauth_redirect_in_stage' | jq -r .Parameter.Value)
+oauth_redirect_out_stage=$(aws ssm get-parameter --name '/sscheck/client/oauth_redirect_out_stage' | jq -r .Parameter.Value)
 
-api_url=$(aws ssm get-parameter --name '	/samplesheet-check/be/rest-api' | jq -r .Parameter.Value)
+api_url=$(aws ssm get-parameter --name '/sscheck/lambda-api' | jq -r .Parameter.Value)
 
 export REACT_APP_API_URL=$api_url
 export REACT_APP_STAGE=dev
