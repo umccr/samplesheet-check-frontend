0961a239c1f1720a8236f4295d098251b9067871
William <Wintan@student.unimelb.edu.au>
2021-09-08 10:05:10 +1000

    Remove Identity Pool

--- react/src/config.js
+++ react/src/config.js
@@ -3,11 +3,13 @@ const REGION = process.env.REACT_APP_REGION;
 const IS_LOCAL = STAGE === "localhost";
 const OAUTH_DOMAIN = `${process.env.REACT_APP_OAUTH_DOMAIN}.auth.${REGION}.amazoncognito.com`;
 
+const LAMBDA_API = `https://${process.env.REACT_APP_LAMBDA_API_DOMAIN}`
+
 const config = {
   // endpoint for backend
   apiGateway: {
     REGION: REGION,
-    URL: process.env.REACT_APP_API_URL,
+    URL: LAMBDA_API,
   },
   cognito: {
     REGION: REGION,
@@ -15,7 +17,6 @@ const config = {
     APP_CLIENT_ID: IS_LOCAL
       ? process.env.REACT_APP_COG_APP_CLIENT_ID_LOCAL
       : process.env.REACT_APP_COG_APP_CLIENT_ID_STAGE,
-    IDENTITY_POOL_ID: process.env.REACT_APP_COG_IDENTITY_POOL_ID,
     OAUTH: {
       domain: OAUTH_DOMAIN,
       scope: ["email", "aws.cognito.signin.user.admin", "openid", "profile"],
