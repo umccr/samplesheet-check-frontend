5ec64c073ec79ac31f2100475dda107072a468cb
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-02-15 18:29:27 +1100

    Add Codebuild Invalidate Cache

--- deploy/stacks/sscheck_front_end_stack.py
+++ deploy/stacks/sscheck_front_end_stack.py
@@ -1,12 +1,16 @@
+import os
 from aws_cdk import (
-    core as cdk,   
+    core as cdk,
     aws_s3 as s3,
     aws_cloudfront as cloudfront,
     aws_ssm as ssm,
     aws_route53 as route53,
     aws_route53_targets as route53t,
-    aws_certificatemanager as acm
+    aws_certificatemanager as acm,
+    aws_codebuild as codebuild,
+    aws_iam as iam,
 )
+
 
 class SampleSheetCheckFrontEndStack(cdk.Stack):
 
@@ -57,47 +61,52 @@ class SampleSheetCheckFrontEndStack(cdk.Stack):
         # Creating bucket for the build directory code
         samplesheet_client_bucket = s3.Bucket(
             self,
-            "umccr-samplesheet-script", 
-            bucket_name = bucket_name,
-            auto_delete_objects = True,
-            removal_policy = cdk.RemovalPolicy.DESTROY,
-            website_index_document = "index.html",
-            website_error_document = "index.html",
-            block_public_access= s3.BlockPublicAccess.BLOCK_ALL
+            "umccr-samplesheet-script",
+            bucket_name=bucket_name,
+            auto_delete_objects=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            website_index_document="index.html",
+            website_error_document="index.html",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
 
         # Create the Origin Access Identity (OAI)
-        cloudfront_oai = cloudfront.OriginAccessIdentity(self, 'umccr-script-oai',
-            comment = "Created By CDK")
-        
+        cloudfront_oai = cloudfront.OriginAccessIdentity(
+            self,
+            'umccr-script-oai',
+            comment="Created By CDK"
+        )
+
         # create s3 configuration details
         s3_origin_source = cloudfront.S3OriginConfig(
-            s3_bucket_source = samplesheet_client_bucket,
-            origin_access_identity = cloudfront_oai                                
+            s3_bucket_source=samplesheet_client_bucket,
+            origin_access_identity=cloudfront_oai
         )
 
         # setup cloudfront config for s3
         source_config = cloudfront.SourceConfiguration(
-            s3_origin_source = s3_origin_source,
+            s3_origin_source=s3_origin_source,
             behaviors=[cloudfront.Behavior(is_default_behavior=True)]
         )
 
         # setup error pages redirection
         error_page_configuration = cloudfront.CfnDistribution.CustomErrorResponseProperty(
-            error_code = 403,
-            error_caching_min_ttl  = 60,
-            response_code = 200,
-            response_page_path = "/index.html"
+            error_code=403,
+            error_caching_min_ttl=60,
+            response_code=200,
+            response_page_path="/index.html"
         )
 
         # create the web distribution
-        sscheck_cloudfront = cloudfront.CloudFrontWebDistribution(self, "cloud_front_name",
+        sscheck_cloudfront = cloudfront.CloudFrontWebDistribution(
+            self,
+            "cloud_front_name",
             origin_configs=[source_config],
-            error_configurations = [error_page_configuration],
-            viewer_protocol_policy = cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            default_root_object = "index.html",
-            price_class = cloudfront.PriceClass.PRICE_CLASS_ALL,
-            enable_ip_v6 = False,
+            error_configurations=[error_page_configuration],
+            viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            default_root_object="index.html",
+            price_class=cloudfront.PriceClass.PRICE_CLASS_ALL,
+            enable_ip_v6=False,
             viewer_certificate=cloudfront.ViewerCertificate.from_acm_certificate(
                 certificate=cert_use1,
                 aliases=props["alias_domain_name"][app_stage],
@@ -105,7 +114,6 @@ class SampleSheetCheckFrontEndStack(cdk.Stack):
                 ssl_method=cloudfront.SSLMethod.SNI
             )
         )
-
 
         # Create A-Record to Route53
         route53.ARecord(
@@ -118,3 +126,39 @@ class SampleSheetCheckFrontEndStack(cdk.Stack):
             record_name="sscheck"
         )
 
+        # Adding codebuild for invalidate CDN cache for every s3 deployment
+        codebuild_build_image = codebuild.Project(
+            self,
+            "CodebuildProjectInvalidateCDNCache",
+            source=codebuild.Source.s3(
+                bucket=samplesheet_client_bucket,
+                path="index.html"
+            ),
+            project_name="InvalidateSSCheckCDNCache",
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0
+            ),
+            build_spec=codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "phases": {
+                    "build": {
+                        "commands": [
+                            """aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*" """
+                        ]
+                    }
+                }
+            }),
+            environment_variables={
+                "DISTRIBUTION_ID": codebuild.BuildEnvironmentVariable(
+                    value=sscheck_cloudfront.distribution_id)
+            }
+        )
+
+        codebuild_build_image.add_to_role_policy(
+            iam.PolicyStatement(
+                resources=[
+                    f"arn:aws:cloudfront::{os.environ.get('CDK_DEFAULT_ACCOUNT')}:distribution/{sscheck_cloudfront.distribution_id}"
+                ],
+                actions=["cloudfront:CreateInvalidation"]
+            )
+        )
