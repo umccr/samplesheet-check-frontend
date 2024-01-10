aed9b832a45db83b0f78adb4592f391f4dec5a97
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-01-10 15:16:31 +1100

    Removal temporary workaround

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
-import { fetchAuthSession } from "aws-amplify/auth";
-import config from "./config.ts";
-
 export function download({ filename, content }) {
   // parameter take a text to be downloaded
   var element = document.createElement("a");
@@ -16,23 +13,4 @@ export function download({ filename, content }) {
   element.click();
 
   document.body.removeChild(element);
-}
-
-// As there is an issue with setting up form-data boundaries with aws-amplify API
-// submitting the samplesheet check will not work so a temporary fetch function declared here
-// until problem is fix
-//
-// Ref: https://github.com/aws-amplify/amplify-js/issues/12638
-
-export async function sscheckFetchApi({ method, additionalHeader, body }) {
-  const authToken = (await fetchAuthSession()).tokens.idToken.toString();
-
-  return await fetch(config.apiGateway.URL, {
-    method: method,
-    headers: {
-      authorization: authToken,
-      ...additionalHeader,
-    },
-    body: body,
-  });
 }
